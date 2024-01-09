c60f2477afe91e9541fdbe6968afb8b3c3aa361b
Jason <quietjason@gmail.com>
2024-01-09 22:03:31 +0800

    Init.

---
+++ README.md
@@ -0,0 +1,6 @@
+## Enable CloudKit
+
+- Got to Project settings > Targets > General > Signing & Capabilites > Background Modes (+ Capability if hasn’t). 
+- Enable “Remove notifications”. 
+- Enable “CloudKit” services. 
+- Add container with id of “iCloud.xxx”.
--- SwiftDataUsage/Todo.swift
+++ SwiftDataUsage/Todo.swift
@@ -9,10 +9,10 @@ import Foundation
 import SwiftData
 
 @Model
-final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
-    }
+final class Todo {
+  var timestamp: Date
+  
+  init(timestamp: Date) {
+    self.timestamp = timestamp
+  }
 }
